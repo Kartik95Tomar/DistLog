2cc373c002164ee5b18d86963b124f623882fd57
Kartik Tomar <kartiktomar95@gmail.com>
2025-07-25 18:54:39 -0700

    Basic Setup

---
+++ src/main/java/com/example/DistLog/DistLogApplication.java
@@ -0,0 +1,13 @@
+package com.example.DistLog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DistLogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DistLogApplication.class, args);
+	}
+
+}
